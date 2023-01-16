37501e793c4464f7df39bf3dd524fa9921e2e97a
Oleg Bukovskiy <olbukovskiy@gmail.com>
2023-01-16 10:23:11 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/React-practice-02/",
   plugins: [react()],
   server: {
-    open: '/index.html',
+    open: "/index.html",
     port: 8000,
   },
 });
